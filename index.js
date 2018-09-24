2021064df78292efd805b8d8b2609caa0106846e
Maxwell Novak <novak.max@gmail.com>
2018-09-24 10:55:55 -0400

    Create a middleware to log requests to server

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var port = process.env.PORT || 8000;
 var app = express();
 var router = express.Router();
 
+
+router.use(function(request, response, next){
+	console.log('Got a request');
+	next();
+});
+
 router.get('/', function(request, response){
 	response.json( 
 	{ 
