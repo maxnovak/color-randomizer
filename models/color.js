8fc8e28a2487baceab1cd981c55cd092d288a234
Maxwell Novak <novak.max@gmail.com>
2018-09-25 13:05:09 -0400

    Create model for colors

---
+++ models/color.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var colorSchema = new Schema({ 
+	name: String,
+	hex: String,
+	rgb: 
+		{
+			red: Number,
+			green: Number,
+			blue: Number,
+		},
+	hsl: 
+	{
+		hue: Number,
+		saturation: Number,
+		lightness: Number,
+	},
+});
+
+module.exports = mongoose.model('color', colorSchema);
