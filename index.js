f42bcec9fbdccf38eec8a52eb52b3ff875b0012f
Maxwell Novak <novak.max@gmail.com>
2018-09-25 13:44:59 -0400

    Add body parser to get request data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var dotenv = require('dotenv');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 8000;
 var app = express();
 var router = require('./router.js');
@@ -14,6 +15,8 @@ app.all('/*', function(request, response, next){
 	console.log('Got a request');
 	next();
 });
+
+app.use(bodyParser.json());
 
 app.use('/', router);
 
