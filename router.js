2990e2ab930ac5880dc94bc5391cdeedd7c4552a
Maxwell Novak <novak.max@gmail.com>
2018-09-25 13:21:14 -0400

    Add post route for adding colors

--- router.js
+++ router.js
@@ -10,3 +10,24 @@ module.exports = router.get('/',
 			response.json(colors);
 		});
 	});
+
+module.exports = router.post('/',
+	function(request, response) {
+		var color = new Color();
+
+		color.name = request.body.name;
+		color.hex = request.body.hex;
+		color.rgb.red = request.body.rgb.red;
+		color.rgb.green = request.body.rgb.green;
+		color.rgb.blue = request.body.rgb.blue;
+		color.hsl.hue = request.body.hsl.hue;
+		color.hsl.saturation = request.body.hsl.saturation;
+		color.hsl.lightness = request.body.hsl.lightness;
+
+		color.save(function(error) {
+			if (error) {
+				response.send(error);
+			}
+			response.json({ message : 'color added' });
+		});
+	});
