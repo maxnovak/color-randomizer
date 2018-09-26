4da35b7fa4cafa915f9287afdb9a5c6ea5e6c08d
Maxwell Novak <novak.max@gmail.com>
2018-09-26 18:59:17 -0400

    Display a random color name on the client

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/color/random');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    console.log(body)
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +29,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response.name}</p>
       </div>
     );
   }
