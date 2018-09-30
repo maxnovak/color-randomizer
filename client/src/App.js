6532fdc8b24810cf2da900c9e08c10b62cd0659e
Maxwell Novak <novak.max@gmail.com>
2018-09-29 21:46:43 -0400

    Remove boilerplate autogenerated header code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -25,11 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro" style={{backgroundColor : this.state.response.hex}}>{this.state.response.name}</p>
+        <p style={{backgroundColor : this.state.response.hex}}>{this.state.response.name}</p>
       </div>
     );
   }
