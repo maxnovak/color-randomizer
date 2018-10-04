cbc09b243814f89902079ffe84b9d8714e4268f6
maxnovak <novak.max@gmail.com>
2018-10-04 12:41:49 -0400

    Formatting

--- client/src/NewColorButton.js
+++ client/src/NewColorButton.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 class NewColorButton extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return(
-            <button onClick={this.getNewColor} style= {{backgroundColor : this.props.colorHex, color : this.props.textColor}}>Get New Color</button>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button
+        onClick={ this.getNewColor }
+        style = {{
+          backgroundColor: this.props.colorHex,
+          color: this.props.textColor
+        }}>
+      Get New Color
+      </button>
+    )
+  }
 }
 
 export default NewColorButton;
