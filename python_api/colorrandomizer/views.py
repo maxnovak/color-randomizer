b1b9823cf27ae7c10743ea65670521d090beadef
maxnovak <novak.max@gmail.com>
2018-10-11 14:45:02 -0400

    Add routes for basic get and post

--- python_api/colorrandomizer/views.py
+++ python_api/colorrandomizer/views.py
@@ -1,5 +1,17 @@
 from colorrandomizer import app
+from flask import request
+import sys, json
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+@app.route("/color", methods=['GET','POST'])
+def color():
+	if request.method == 'POST':
+		return post_data(request)
+	else:
+		return get_data()
+
+def post_data(request):
+	print(request.json, file=sys.stderr)
+	return "Data successfully Posted"
+
+def get_data():
+	return "Got Data"
