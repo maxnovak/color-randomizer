28237af2ed9711d5bf7fbafdfc9f8358b15d33c2
Maxwell Novak <novak.max@gmail.com>
2018-10-12 14:57:38 -0400

    Correct project name in setup.py

--- python_api/setup.py
+++ python_api/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='yourapplication',
-    packages=['yourapplication'],
+    name='colorrandomizer',
+    packages=['colorrandomizer'],
     include_package_data=True,
     install_requires=[
         'flask',
