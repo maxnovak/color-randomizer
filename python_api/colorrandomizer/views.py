df9879012f2d2f7a036c15e02264c86abcf06f98
Maxwell Novak <novak.max@gmail.com>
2018-10-18 16:24:43 -0400

    Add validation errors to schema and return 400 if invalid

--- python_api/colorrandomizer/views.py
+++ python_api/colorrandomizer/views.py
@@ -2,19 +2,19 @@ from colorrandomizer import app, mongo
 from flask import request
 import sys, json
 from random import *
-from schema import Schema, And, Use, Optional
+from schema import Schema, And, Use, SchemaError
 
-schema = Schema({'name': Use(str),
-	'hex': Use(str),
+schema = Schema({'name': Use(str, error='Invalid name'),
+	'hex': Use(str, error='Invalid Hex value'),
 	'rgb': {
-		'red': Use(int),
-		'green': Use(int),
-		'blue': Use(int)
+		'red': Use(int, error='Invalid value'),
+		'green': Use(int, error='Invalid value'),
+		'blue': Use(int, error='Invalid value')
 	},
 	'hsl': {
-		'hue': Use(int),
-		'saturation': Use(int),
-		'lightness': Use(int)
+		'hue': Use(int, error='Invalid value'),
+		'saturation': Use(int, error='Invalid value'),
+		'lightness': Use(int, error='Invalid value')
 	}
 	})
 
@@ -27,9 +27,23 @@ def color():
 
 def post_data(request):
 	print(request.json, file=sys.stderr)
-	data = schema.validate(request.json)
+	try:
+		data = schema.validate(request.json)
+	except SchemaError:
+		print(SchemaError, file=sys.stderr)
+		return bad_request()
 	mongo.db.colors.insert_one(request.json)
 	return "Data successfully Posted"
+
+@app.errorhandler(400)
+def bad_request():
+	message = {
+		'status':400,
+		'message': 'invalid request',
+	}
+	resp = json.dumps(message)
+
+	return resp
 
 def get_all_colors():
 	colors = mongo.db.colors.find(projection={'_id':False, '__v':False})
