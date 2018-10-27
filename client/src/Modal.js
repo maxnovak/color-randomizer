5ef9b0124639229bc1181046fd1e2e04db5e4456
maxnovak <novak.max@gmail.com>
2018-10-26 22:21:22 -0400

    Make the modal into a form

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -7,10 +7,16 @@ class Modal extends Component {
 
 	render() {
 		return (
-      <div style={{display: this.props.visible}}>
-        blahblahblah
-        <input />
-      </div>
+      <form style={{display: this.props.visible}}>
+        Color Name: <input type="text" name="name" />
+        Hex Value: <input type="text" name="hex" />
+        Red Value: <input type="text" name="red" />
+        Green Value: <input type="text" name="green" />
+        Blue Value: <input type="text" name="blue" />
+        Hue Value: <input type="text" name="hue" />
+        Saturation Value: <input type="text" name="saturation" />
+        Lightness Value: <input type="text" name="lightness" />
+      </form>
 		)
 	}
 
